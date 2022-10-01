e965d6385bc74a6d2ecc9aded9e075680ece7759
Abinavraj5427 <abinavraj10@gmail.com>
2022-10-01 16:14:49 -0500

    Got an API backend

---
+++ api/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const api = express();
+
+const port = 8080;
+
+api.use(express.json())
+api.use(express.urlencoded());
+api.use(cors({
+    origin: '*'
+}));
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+// api.get('/', (req, res) => {
+//     console.log("Hello World");
+// });
+
+api.post('/generate', (req, res) => {
+    if(!req.body.story){
+        console.log("Received story: "+ req.body.story);
+    }
+
+    var story = req.body.story;
+    var splitLines = story.match(/[^\.!\?]+[\.!\?]+/g);
+    console.log(splitLines);
+
+    var hash = makeid(5);
+    var filename = hash + ".txt";
+    var filedata = "";
+    splitLines.forEach(line => {
+        filedata = filedata + line.trim() + "\n"
+    });
+    filedata = filedata.trim();
+    fs.writeFile("story-text/"+filename, filedata, function (err) {
+        if (err) return console.log(err);
+        console.log(hash + '.txt > helloworld.txt')
+    });
+    return "Success";
+});
+
+api.listen(port, () => console.log(`StoryTime api listening on port ${port}!`))
