4f09bde02f4bcf078742209e52ca1b443ed5e8b7
harishanmugaraja <harishanmugaraja@gmail.com>
2022-10-02 04:43:59 -0400

    downloads working

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const path = require('path');
 const { exec } = require('node:child_process');
+// const exec = require('await-exec');
 const api = express();
-
 const port = 8080;
+var most_recent_hash;
 
 api.use(express.json())
 api.use(express.urlencoded());
@@ -23,7 +25,16 @@ function makeid(length) {
    return result;
 }
 
-api.post('/generate', (req, res) => {
+api.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+api.get('/download', (req, res) => {
+  res.download("stories/"+most_recent_hash+"/movie.avi")
+});
+
+api.post('/generate', async (req, res) => {
+
     if(!req.body.story){
         console.log("Received story: "+ req.body.story);
     }
@@ -33,6 +44,7 @@ api.post('/generate', (req, res) => {
     console.log(splitLines);
 
     var hash = makeid(5);
+    most_recent_hash = hash;
     var filename = "prompts.txt";
     var filedata = "";
     splitLines.forEach(line => {
@@ -44,6 +56,13 @@ api.post('/generate', (req, res) => {
         if (err) return console.log(err);
         console.log('Wrote to file!')
     });
+
+
+    // await exec('python scripts/txt2img.py --from-file stories/'+hash+'/prompts.txt --ddim_steps 128 --scale 7.5 --H 256 --W 320 --n_iter 1 --n_samples 1 --outdir stories/'+hash+' --seed '+ (Math.floor(Math.random() * 10000000) + 1)+'');
+
+    // await exec('python generator.py '+hash+'');
+
+
     exec('python scripts/txt2img.py --from-file stories/'+hash+'/prompts.txt --ddim_steps 128 --scale 7.5 --H 256 --W 320 --n_iter 1 --n_samples 1 --outdir stories/'+hash+' --seed '+ (Math.floor(Math.random() * 10000000) + 1)+'' , (err, output) => {
       // once the command has completed, the callback function is called
       if (err) {
@@ -62,36 +81,13 @@ api.post('/generate', (req, res) => {
         }
         // log the output received from the command
         console.log("Output: \n", output)
-
+        console.log("downloading now!")
+        res.redirect('/download');
       });
     });
-      // exec('python scripts/txt2img.py --from-file stories/'+hash+'/prompts.txt --H 256 --W 256 --n_iter 1 --n_samples 1 --outdir stories/'+hash+'', (err, output) => {
-      //   // once the command has completed, the callback function is called
-      //   if (err) {
-      //       // log and return if we encounter an error
-      //       console.error("could not execute command: ", err)
-      //       return
-      //   }
-      //   // log the output received from the command
-      //   console.log("Output: \n", output)
-      //
-      // })
 
 
-    //   exec('', (err, output) => {
-    //     // once the command has completed, the callback function is called
-    //     if (err) {
-    //         // log and return if we encounter an error
-    //         console.error("could not execute command: ", err)
-    //         return
-    //     }
-    //     // log the output received from the command
-    //     console.log("Output: \n", output)
-    //
-    //   })
-    // })
 
-    return "Success";
 });
 
 api.listen(port, () => console.log(`StoryTime api listening on port ${port}!`))
